94c4144ff741da23868711a0c83a4ad2a04ee8ee
whroman <walterhumberto.roman@gmail.com>
2014-05-04 16:13:43 -0700

    Solutions for "Machine Language" chapter

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,74 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// -1 produces a binary number that will fill up all 16 bits of a word
+
+    @SCREEN
+    D = A
+    @pointer
+    M = D
+
+    @8191
+    D = A
+    @endAt
+    M = D
+    @pointer
+    D = M
+    @endAt
+    M = M + D
+
+(RESET)
+    @SCREEN
+    D = A
+    @pointer
+    M = D
+
+    @KBD
+    D = M
+    @LISTENKBD
+    D;JEQ
+    @LOOP
+    D;JNE
+
+(LISTENKBD)
+    @pointer
+    A = M
+    M = 0
+    D = A
+
+    @pointer
+    M = M + 1
+
+    @endAt
+    D = M - D
+    @RESET
+    D;JLE
+
+    @KBD
+    D = M
+    @LISTENKBD
+    D;JEQ
+    @LOOP
+    D;JNE
+
+
+(LOOP)
+    @pointer
+    A = M
+    M = -1
+    D = A
+
+    @pointer
+    M = M + 1
+
+    @endAt
+    D = M - D
+    @RESET
+    D;JLE   
+
+    @KBD
+    D = M
+    @LISTENKBD
+    D;JEQ
+    @LOOP
+    D;JNE
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+    // Define counter, @i
+    @i
+    M=0
+
+    // Initialize @R2 as 0
+    @R2
+    M=0
+
+// Loop @R0 times
+(LOOP)
+    // Call counter
+    @i
+    D=M // D = i
+
+    // Check if loop should continue or end
+    @R0
+    D=M - D // D = @R0 - i
+    @END
+    D;JLE // If (@R0 - i <= 0) goto END
+    
+    // Add to @R2
+    @R1
+    D=M // D = R1
+    @R2
+    M=M+D // R2 = R2 + R1
+
+    // Increment counter, @i
+    @i
+    M=M+1 // i = i + 1
+    
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
